98ca0fe6217a562a0de77edb4f7bfe65f3d86d97
Ailin Druzic <ailindruzic@gmail.com>
2023-06-08 10:46:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import randomcolor from "randomcolor";
 
 function App() {
+  const [lockers, setLockers] = useState(
+    Array(10).fill({ status: "free", color: "" })
+  );
+  const [nextLockerIndex, setNextLockerIndex] = useState(0);
+
+  const reserveLocker = () => {
+    setLockers((prevLockers) => {
+      const newLockers = [...prevLockers];
+      newLockers[nextLockerIndex] = { status: "taken", color: randomcolor() };
+      const updatedIndex =
+        nextLockerIndex + 2 < lockers.length ? nextLockerIndex + 2 : 1;
+      setNextLockerIndex(updatedIndex);
+      return newLockers;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="mt-4 pl-8">
+      <h2 className="font-sans text-4xl mt-4">Gym Locker Reservation</h2>
+      <button
+        className="mt-4 px-5 bg-teal-700 rounded-lg text-slate-100"
+        onClick={reserveLocker}
+      >
+        Reserve Locker
+      </button>
+      <div className="mt-4 grid grid-cols-4 gap-4"></div>
+      {lockers.map((locker, index) => (
+        <button
+          key={index}
+          className={`locker ${locker.status}  rounded-lg p-4 border border-gray-600 border-opacity-100`}
+          disabled={locker.status === "taken"}
+          style={{ backgroundColor: locker.color, paddingBottom: "15%" }}
+          onClick={locker.status === "free" ? reserveLocker : null}
         >
-          Learn React
-        </a>
-      </header>
+          #{index + 1}
+          <br />
+          {locker.status === "taken" ? "Taken" : "Free"}
+        </button>
+      ))}
     </div>
   );
 }
